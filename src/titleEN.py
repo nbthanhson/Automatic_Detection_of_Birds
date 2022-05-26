574d97f281a6f9bfea54e2e8973116e8e1062182
Son Nguyen <83537172+nbthanhson@users.noreply.github.com>
2022-05-26 22:00:10 +0700

    Add files via upload

---
+++ src/titleEN.py
@@ -0,0 +1,152 @@
+CHEF_INFO = """
+<h2 class="font-title">AUTOMATIC DETECTION OF BIRDS! </h2>
+<p class="strong font-body">
+<span class="d-block extra-info"><em>Upload your image or choose from available images. 
+The image you select or upload will be processed by ADB's deep learning model in real-time and the output will be displayed on the screen.</em></span>
+</p>
+""".strip()
+
+SIDEBAR_INFO = """
+<div class="contributors font-body text-bold">
+<a class="contributor comma" href="https://huggingface.co/m3hrdadfi">Mehrdad Farahani</a>
+<a class="contributor comma" href="https://huggingface.co/dk-crazydiv">Kartik Godawat</a>
+<a class="contributor comma" href="https://huggingface.co/hassiahk">Haswanth Aekula</a>
+<a class="contributor comma" href="https://huggingface.co/rays2pix">Deepak Pandian</a>
+<a class="contributor comma" href="https://huggingface.co/nbroad">Nicholas Broad</a>
+</div>
+"""
+
+GITHUB_ICON = """
+<p>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" width="18" height="18">
+<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z">
+</path></svg>
+<a style="text-decoration: none; color: #31333F" href="https://github.com/nbthanhson">github.com/nbthanhson </a>
+</p>
+""".strip()
+
+LINKEDIN_ICON = """
+<p>
+<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 35 35"
+style=" fill:#000000;">    <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z">
+</path></svg>
+<a style="text-decoration: none; color: #31333F" href="https://www.linkedin.com/in/nbthanhson/">linkedin.com/in/nbthanhson </a>
+</p>
+""".strip()
+
+STORY = """
+<div class="story-box font-body">
+<p>
+<strong>Step 1: </strong>Select an image in one of two ways: <em>"Upload an image"</em> or <em>"Choose from available images"</em>.<br />
+<strong>Step 2: </strong>Click <em>"Get results!"</em> for ADB to detect and classify objects.<br />
+<strong>Step 3: </strong>View the results and description of detected objects.<br /><br />
+<em>For more information about ADB or how to contact, please select from the left sidebar of the screen.</em>
+</p>
+</div>
+""".strip()
+
+EAGLE = """
+<div class="story-box font-body">
+<p>
+Eagle is the common name for many large birds of prey of the family Accipitridae. 
+Eagles belong to several groups of genera, some of which are closely related. 
+Most of the 60 species of eagle are from Eurasia and Africa. 
+Outside this area, just 14 species can be found — 2 in North America, 9 in Central and South America, and 3 in Australia.
+<br /><br />
+Eagles aren't a natural group but denote essentially any bird of prey large enough to hunt sizeable (about 50cm long or more overall) vertebrate.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Eagle"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Eagle:
+</p>
+</div>
+""".strip()
+
+FLAMINGO = """
+<div class="story-box font-body">
+<p>
+Flamingos or flamingoes are a type of wading bird in the family Phoenicopteridae, 
+which is the only extant family in the order Phoenicopteriformes. 
+There are four flamingo species distributed throughout the Americas (including the Caribbean), 
+and two species native to Africa, Asia, and Europe.
+<br /><br />
+The name flamingo comes from Portuguese or Spanish flamengo ("flame-colored"), 
+which in turn comes from Provençal flamenc - a combination of flama ("flame") and a Germanic-like suffix -ing.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Flamingo"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Flamingo:
+</p>
+</div>
+""".strip()
+
+GOLDEN_ORIOLE = """
+<div class="story-box font-body">
+<p>
+The Eurasian golden oriole (Oriolus oriolus) also called golden oriole, 
+is the only member of the Old World oriole family of passerine birds breeding in Northern Hemisphere temperate regions. 
+It is a summer migrant in Europe and Palearctic and spends the winter season in central and southern Africa.
+<br /><br />
+Golden orioles have an extremely large range with large populations that are apparently stable. 
+Therefore, they are evaluated as least concern by BirdLife International.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Eurasian_golden_oriole"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Golden Oriole:
+</p>
+</div>
+""".strip()
+
+HYACINTH_MACAW = """
+<div class="story-box font-body">
+<p>
+The hyacinth macaw (Anodorhynchus hyacinthinus), or hyacinthine macaw, is a parrot native to central and eastern South America. 
+With a length (from the top of its head to the tip of its long pointed tail) of about one meter it is longer than any other species of parrot. 
+It is the largest macaw and the largest flying parrot species; the flightless kākāpō of New Zealand outweighs it at up to 3.5 kg. 
+While generally easily recognized, it could be confused with the smaller Lear's macaw.
+<br /><br />
+Habitat loss and the trapping of wild birds for the pet trade have taken a heavy toll on their population in the wild, 
+so the species is classified as Vulnerable on the International Union for Conservation of Nature's Red List, 
+and it is protected by its listing on Appendix I of the Convention on International Trade in Endangered Species of Wild Fauna and Flora.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Hyacinth_macaw"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Hyacinth Macaw:
+</p>
+</div>
+""".strip()
+
+MUTE_SWAN = """
+<div class="story-box font-body">
+<p>
+The mute swan (Cygnus olor) is a species of swan and a member of the waterfowl family Anatidae. 
+It is native to much of Eurosiberia, and the far north of Africa. 
+It is an introduced species in North America, home to the largest populations outside of its native range, 
+with additional smaller introductions in Australasia and southern Africa. 
+The name 'mute' derives from it being less vocal than other swan species.
+<br /><br />
+Measuring 125 to 170cm in length, this large swan is wholly white in plumage with an orange beak bordered with black. 
+It is recognisable by its pronounced knob atop the beak, which is larger in males.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Mute_swan"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Mute Swan:
+</p>
+</div>
+""".strip()
+
+SEAGULL = """
+<div class="story-box font-body">
+<p>
+Gulls, or colloquially seagulls, are seabirds of the family Laridae in the suborder Lari. 
+They are most closely related to the terns and skimmers and only distantly related to auks, and even more distantly to waders. 
+Until the 21st century, most gulls were placed in the genus Larus, but that arrangement is now considered polyphyletic, leading to the resurrection of several genera.
+<br /><br />
+An older name for gulls is mews, which is cognate with German Möwe, Danish måge, Swedish mås, Dutch meeuw, Norwegian måke/måse and French mouette, and can still be found in certain regional dialects.
+<br /><br />
+Click <a style="text-decoration: none; color: #31333F" href="https://en.wikipedia.org/wiki/Gull"><strong>here</strong></a> to get more information!
+<br /><br />
+Here are three other images of the Seagull:
+</p>
+</div>
+""".strip()
